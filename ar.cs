e0fa28c0063021ba40e11faf0d89d026bd93b05d
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-06 14:38:02 +0300

    C#

--- ar.cs
+++ ar.cs
@@ -5,29 +5,33 @@ class Program
     static void Main()
     {
         // Get the size of the array from the user
-        Console.Write("Enter the size of the array: ");
+        Console.Write("Enter the number of subjects: ");
         int size = int.Parse(Console.ReadLine());
-        int limit=int.Parse(Console.ReadLine();)
 
         // Declare an array to store user input
-        int[] numbers = new int[size];
-        int[] numbers = new int[limit];
+        int[] marks = new int[size];
 
-        // Get elements from the user and store them in the array
-        for (int i = 0; i>= 89; i < size; i++)
+        // Get marks from the user and store them in the array
+        for (int i = 0; i < size; i++)
         {
-            Console.Write($"Enter marks {i + 1}: ");
-            numbers[i] = int.Parse(Console.ReadLine());
-            Console.Write($"Enter marks Limit {i + 1}: ");
-            numbers[i] = int.Parse(Console.ReadLine());
+            Console.Write($"Enter marks for subject {i + 1}: ");
+            int mark = int.Parse(Console.ReadLine());
 
+            // Check if the mark is 100 or greater
+            if (mark >= 100)
+            {
+                Console.WriteLine("Marks cannot be 100 or more. Exiting program.");
+                return;
+            }
+
+            marks[i] = mark;
         }
 
-        // Calculate the sum and average of elements
+        // Calculate the sum and average of marks
         int sum = 0;
-        foreach (int num in numbers)
+        foreach (int mark in marks)
         {
-            sum += num;
+            sum += mark;
         }
         double average = (double)sum / size;
 
@@ -36,8 +40,8 @@ class Program
 
         // Display the results
         Console.WriteLine("\nResults:");
-        Console.WriteLine($"Sum: {results[0]}");
-        Console.WriteLine($"Average: {results[1]}");
+        Console.WriteLine($"Sum of Marks: {results[0]}");
+        Console.WriteLine($"Average Marks: {results[1]:0.00}");
         Console.WriteLine($"Total number of subjects: {results[2]}");
     }
 }
