6e25d864183ba71f2078f8d9cb692349132866f7
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-07 12:09:03 +0300

    C#

---
+++ meth.cs
@@ -0,0 +1,17 @@
+using System;
+namespace trialmeth
+{
+    public static class TrialClass
+    {
+        public static float ComputeSum()
+        {
+            float a = 6.6f;
+            float b = 67.1f;
+            float c = 0.6f;
+            float e = 9.1f;
+            
+            float sum = a + b + c + e;
+            return sum;
+        }
+    }
+}
