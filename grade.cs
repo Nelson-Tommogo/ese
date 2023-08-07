32032825e157d5f3df4028bd582f954c016201e5
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-07 10:40:17 +0300

    C#

---
+++ grade.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace grading{
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a letter: ");
+        char letter = char.ToUpper(Console.ReadKey().KeyChar);
+
+        string carModel;
+
+        switch (letter)
+        {
+            case 'T':
+                carModel = "Toyota";
+                break;
+            case 'M':
+                carModel = "Mercedes";
+                break;
+            case 'R':
+                carModel = "Range Rover";
+                break;
+            case 'L':
+                carModel = "Lexus";
+                break;
+            default:
+                carModel = "Car not in the database";
+                break;
+        }
+
+        Console.WriteLine($"\nCar Model: {carModel}");
+    }
+}
+}
