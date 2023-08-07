35b574848b07bc7b0c6af571964a0796941bd7e3
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-07 12:21:35 +0300

    C#

--- meth.cs
+++ meth.cs
@@ -1,4 +1,5 @@
 using System;
+
 namespace trialmeth
 {
     public static class TrialClass
@@ -9,9 +10,19 @@ namespace trialmeth
             float b = 67.1f;
             float c = 0.6f;
             float e = 9.1f;
-            
+
             float sum = a + b + c + e;
             return sum;
+        }
+    }
+
+    public class Program // Separate class for Main method
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calling the ComputeSum method:");
+            float result = TrialClass.ComputeSum();
+            Console.WriteLine("Sum: " + result);
         }
     }
 }
